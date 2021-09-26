8c1af5b5617ce59b4ac25abe52da9e1b19c57e11
wyzip1 <50360177+wyzip1@users.noreply.github.com>
2021-09-26 19:59:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ export function getType(data) {
 
 export function getQuery() {
   const query = {};
-  const list = location.search ? location.search.split('?')[1] : [];
+  const list = location.search ? location.search.split('?')[1].split('&') : [];
   for(let i of list) {
     if(!i) continue;
-    const key = i.split('&')[0];
-    const value = i.split('&')[1];
+    const key = i.split('=')[0];
+    const value = i.split('=')[1];
     query[key] = value;
   }
   return query;
