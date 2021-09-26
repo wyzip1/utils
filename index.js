741f8123076ece7642d647e9d26bf7e5c976af23
wyzip1 <50360177+wyzip1@users.noreply.github.com>
2021-09-26 19:57:10 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+export function getType(data) {
+  const type = Object.prototype.toString.call(data);
+  switch (type) {
+    case '[object Undefined]': return 'undefined';
+    case '[object Null]': return 'null';
+    case '[object Boolean]': return 'boolean';
+    case '[object Function]': return 'function';
+    case '[object Object]': return 'object';
+    case '[object Array]': return 'array';
+    case '[object String]': return 'string';
+    case '[object Number]': return 'number';
+    case '[object Promise]': return 'Promise'
+    case '[object RegExp]': return 'RegExp';
+    case '[object Symbol]': return 'Symbol';
+    case '[object Date]': return 'Date';
+    case '[object Map]': return 'Map';
+    case '[object Set]': return 'Set';
+    case '[object WeakMap]': return 'WeakMap';
+    case '[object WeakSet]': return 'WeakSet';
+    default: return type;
+  }
+}
+
+export function getQuery() {
+  const query = {};
+  const list = location.search ? location.search.split('?')[1] : [];
+  for(let i of list) {
+    if(!i) continue;
+    const key = i.split('&')[0];
+    const value = i.split('&')[1];
+    query[key] = value;
+  }
+  return query;
+}
